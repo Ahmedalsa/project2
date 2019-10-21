806dd5ec4365a7acbf1e444441513a5d7e28b4ee
Ahmedalsa <42217218+Ahmedalsa@users.noreply.github.com>
2019-10-21 09:14:03 +0300

    add the decorators module file

--- application.py
+++ application.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask
 from flask_socketio import SocketIO, emit
+from decorators import login_required
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
@@ -10,4 +11,11 @@ socketio = SocketIO(app)
 
 @app.route("/")
 def index():
-    
+
+@app.route("/")
+def login():
+
+
+@app.route("/")
+@login_required
+def logout():
