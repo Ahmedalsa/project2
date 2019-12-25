a498190d0f8e26b5883a3500b8ef4a5143df0e64
Ahmedalsa <42217218+Ahmedalsa@users.noreply.github.com>
2019-12-25 11:00:31 +0300

    adding more variables and methods to index.js

--- static/index.js
+++ static/index.js
@@ -8,3 +8,21 @@ document.addEventListener('DOMContentLoaded', () => {
       if (!localStorage.getItem('nickname'))
         localStorage.setItem('nickname', nickname));
       }
+      // Connect to websocket
+      var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+
+      // When connected, configure buttons
+      socket.on('connect', () => {
+
+          // TODO Replace function below
+          // Each button should emit a "submit vote" event
+          document.querySelectorAll('button').forEach(button => {
+              button.onclick = () => {
+                  const selection = button.dataset.vote;
+                  socket.emit('submit vote', {'selection': selection});
+              };
+          });
+      });
+
+    });
+});
